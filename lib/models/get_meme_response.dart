c79c5d128512c08f5e39e2415afa4102e1171aca
akhilsomanvs <akhilsoman426@gmail.com>
2021-10-28 17:25:13 +0530

    Created api repository.

---
+++ lib/models/get_meme_response.dart
@@ -0,0 +1,74 @@
+class GetMemeResponseModel {
+  GetMemeResponseModel({
+    required this.success,
+    required this.data,
+  });
+  late final bool success;
+  late final Data data;
+
+  GetMemeResponseModel.fromJson(Map<String, dynamic> json){
+    success = json['success'];
+    data = Data.fromJson(json['data']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['success'] = success;
+    _data['data'] = data.toJson();
+    return _data;
+  }
+}
+
+class Data {
+  Data({
+    required this.memes,
+  });
+  late final List<Memes> memes;
+
+  Data.fromJson(Map<String, dynamic> json){
+    memes = List.from(json['memes']).map((e)=>Memes.fromJson(e)).toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['memes'] = memes.map((e)=>e.toJson()).toList();
+    return _data;
+  }
+}
+
+class Memes {
+  Memes({
+    required this.id,
+    required this.name,
+    required this.url,
+    required this.width,
+    required this.height,
+    required this.boxCount,
+  });
+  late final String id;
+  late final String name;
+  late final String url;
+  late final int width;
+  late final int height;
+  late final int boxCount;
+
+  Memes.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    name = json['name'];
+    url = json['url'];
+    width = json['width'];
+    height = json['height'];
+    boxCount = json['box_count'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['id'] = id;
+    _data['name'] = name;
+    _data['url'] = url;
+    _data['width'] = width;
+    _data['height'] = height;
+    _data['box_count'] = boxCount;
+    return _data;
+  }
+}
---
+++ lib/repositories/api_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:image_flip/repositories/api/api_exception.dart';
+
+class ApiRepository {
+  final String _apiUrl = "https://api.imgflip.com/get_memes";
+
+  Future<dynamic> fetchMemes() async {
+    var responseJson;
+    try {
+      final response = await http.get(Uri.parse(_apiUrl));
+      responseJson = _returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet connection');
+    }
+    return responseJson;
+  }
+
+  dynamic _returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = json.decode(response.body.toString());
+        print(responseJson);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+    }
+  }
+}

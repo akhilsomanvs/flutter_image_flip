3ea1602bc8f32740adfe894c82b8e3fb22f8d4e7
akhilsomanvs <akhilsoman426@gmail.com>
2021-10-29 16:41:28 +0530

    Updating meme list in controller.

--- lib/controllers/meme_controller.dart
+++ lib/controllers/meme_controller.dart
@@ -8,9 +8,12 @@ class MemeController extends ChangeNotifier {
 
   GetMemeResponseModel? memeResponseModel;
 
+  List<Memes> _memesList = [];
   List<Memes> _savedMemesList = [];
 
-  List<Memes> get savedMemesList => _savedMemesList;
+  List<Memes> get memeList => _memesList;
+
+  get savedMemesList => _memesList.where((e) => e.isSaved).toList();
 
   getMemes() async {
     showLoading = true;
@@ -20,6 +23,16 @@ class MemeController extends ChangeNotifier {
     final response = await RemoteDataSource().fetchMemeMetaData();
     if (response.data != null && response.data is GetMemeResponseModel) {
       memeResponseModel = response.data;
+      if (memeResponseModel != null) {
+        _memesList = memeResponseModel!.data.memes;
+      }
+    }
+    await getSavedMemes();
+    for (var meme in _memesList) {
+      final index = _savedMemesList.indexWhere((element) => element.id == meme.id);
+      if (index != -1) {
+        meme.isSaved = true;
+      }
     }
     showLoading = false;
     notifyListeners();
@@ -27,16 +40,24 @@ class MemeController extends ChangeNotifier {
 
   saveMeme(Memes meme) async {
     await LocalDataSource.saveMemeToDB(meme);
+    meme.isSaved = true;
+    _savedMemesList.add(meme);
     notifyListeners();
   }
 
-  removeMeme(Memes meme)async{
+  removeMeme(Memes meme) async {
     await LocalDataSource.removeMemeFromDB(meme);
+    meme.isSaved = false;
+    _savedMemesList.remove(meme);
     notifyListeners();
   }
 
   getSavedMemes() async {
     _savedMemesList = await LocalDataSource.getSavedMemes();
+    for (var element in _memesList) {
+      final index = _savedMemesList.indexWhere((meme) => meme.id == element.id);
+      element.isSaved = index != -1;
+    }
     notifyListeners();
   }
 }

9d36a4fbcdde9b9152158f35a2087722c1df858c
akhilsomanvs <akhilsoman426@gmail.com>
2021-10-29 12:33:39 +0530

    added methods for adding and remove memes

--- lib/data/dao/meme_dao.dart
+++ lib/data/dao/meme_dao.dart
@@ -2,20 +2,23 @@ import 'package:image_flip/data/service/app_database.dart';
 import 'package:image_flip/models/get_meme_response.dart';
 import 'package:sembast/sembast.dart';
 
-
-class MemeDao{
+class MemeDao {
   static const String MEME_STORE_NAME = "MEMES";
   final _memeStore = intMapStoreFactory.store(MEME_STORE_NAME);
+
   Future<Database> get _db async => await AppDatabase.instance.database;
 
-  Future insert (Memes meme)async{
+  Future insert(Memes meme) async {
     await _memeStore.add(await _db, meme.toJson());
   }
 
-  Future update(Memes meme)async{
+  Future update(Memes meme) async {
     final finder = Finder(filter: Filter.byKey(meme.id));
-    await _memeStore.update(await _db, meme.toJson(),finder: finder);
+    await _memeStore.update(await _db, meme.toJson(), finder: finder);
   }
 
-
+  Future delete(Memes meme) async {
+    final finder = Finder(filter: Filter.byKey(meme.id));
+    await _memeStore.delete(await _db, finder: finder);
+  }
 }
--- lib/data/dataSources/local_data_source.dart
+++ lib/data/dataSources/local_data_source.dart
@@ -1,3 +1,14 @@
-class LocalDataSource{
+import 'package:image_flip/data/dao/meme_dao.dart';
+import 'package:image_flip/models/get_meme_response.dart';
 
+class LocalDataSource {
+  static final MemeDao _dao = MemeDao();
+
+  static saveMemeToDB(Memes meme) async {
+    await _dao.insert(meme);
+  }
+
+  static removeMemeFromDB(Memes meme) async {
+    await _dao.delete(meme);
+  }
 }
